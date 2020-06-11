9759fabd30e1017d81893766b608ad30a6ff480a
Akamai <66426042+Akamai98@users.noreply.github.com>
2020-06-10 22:00:24 -0300

    Mejora en testeo de nombres ingresados

--- src/test/java/CodeBusters/BCTest.java
+++ src/test/java/CodeBusters/BCTest.java
@@ -5,17 +5,21 @@ import org.junit.BeforeClass;
 public class BCTest {
     private static PedirNombres pedir;
     private static String n1, n2;
+    private static boolean expected;
 
     @BeforeClass
     public static void beforeClass() {
         pedir = new PedirNombres();
+
         n1 = "nombreJugador1";
         n2 = "nombreJugador2";
+
+        expected = true;
     }
 
     @Test
     public void testNombres() {
         boolean actual = pedir.chequearNombres(n1,n2);
-        Assert.assertEquals(true,actual);
+        Assert.assertEquals(expected,actual);
     }
 }
