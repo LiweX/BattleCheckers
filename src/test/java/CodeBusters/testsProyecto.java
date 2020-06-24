771cd7a3b76dc201ecd8240b27fa427d27264df4
Lihue Leandro Luna <62813313+LiweX@users.noreply.github.com>
2020-06-23 22:54:07 -0300

    Update testsProyecto.java

--- src/test/java/CodeBusters/testsProyecto.java
+++ src/test/java/CodeBusters/testsProyecto.java
@@ -2,43 +2,20 @@ import org.junit.Test;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 
-
-import javax.swing.ImageIcon;
-import java.util.ArrayList;
-
 public class testsProyecto {
 
     private static String nombre1, nombre2, nombre3;
     private static PedirNombres pedirNombres;
-    private static Celda testSelectionNull, testMovement;
-    private static Tablero tablero;
-    private static Estadisticas stats;
-    private static Juego juego;
-    private static ImageIcon fichaBlanca, fichaRoja, vacio;
-    private static Ficha fichaTest, fichaMovementTest;
-    private static ArrayList<Celda> contiguas, eliminar;
+
 
     @BeforeClass
     public static void beforeClass() {
         nombre1 = "jugador1";
         nombre2 = "jugador2";
         nombre3 = "";
-        
-        fichaBlanca = new ImageIcon("src/resources/fichaBlanca.png");
-        fichaRoja = new ImageIcon("src/resources/fichaRoja.png");
-        vacio = new ImageIcon("");
-        
-        tablero = new Tablero();
-        
-        stats = new Estadisticas();
-        
-        juego = new Juego(tablero,stats);
 
         pedirNombres = new PedirNombres();
-        
-        testSelectionNull = new Celda(0,1);
-        
-        fichaTest = new Ficha(ColorFicha.BLANCA,fichaBlanca);
+
     }
 
     //Test que evalua distintos casos de inputs de nombres.
